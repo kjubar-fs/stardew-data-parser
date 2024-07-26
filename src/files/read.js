6807ad463c57f2a613a0c7eff2fdd87c54bc3914
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-07-26 18:28:57 -0400

    Reorganize code structure

---
+++ src/files/read.js
@@ -0,0 +1,14 @@
+/*
+ *  Author: Kaleb Jubar
+ *  Created: 26 Jul 2024, 5:17:35 PM
+ *  Last update: 26 Jul 2024, 5:54:15 PM
+ *  Copyright (c) 2024 Kaleb Jubar
+ */
+/**
+ * Load a raw JSON file as an object 
+ * @param dir directory to load from
+ * @param file filename to load (no extension)
+ */
+export function loadRawJson(dir: String, file: String): any | null {
+    console.log(`Loading raw JSON from ${dir}/${file}.escaped.json...`)
+}
