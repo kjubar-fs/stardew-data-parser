11711ebf66354588d5a2308120aba2950855084d
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-07-29 13:14:20 -0400

    Minor changes to read JSON return values

--- src/files/read.js
+++ src/files/read.js
@@ -1,29 +1,36 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 26 Jul 2024, 5:17:35 PM
- *  Last update: 29 Jul 2024, 12:22:17 PM
+ *  Last update: 29 Jul 2024, 1:13:44 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import path from "node:path";
 import { readFileSync } from "node:fs";
 
 const DATA_DIRECTORY = "../../raw-data-escaped";
+// use import.meta.dirname because we don't have access to __dirname in modules
 const curDir = import.meta.dirname
 
 /**
  * Load a raw JSON file as an object
  * @param {string} dir directory to load from
  * @param {file} file filename to load (no extension)
+ * @returns a JavaScript object formed from the loaded JSON
  */
 export function loadRawJson(dir, file) {
+    // create full qualified path
     const filename = `${file}.json`;
     const fullPath = path.join(curDir, DATA_DIRECTORY, dir, filename);
-    console.log(`Loading raw JSON from ${fullPath}`)
+    console.debug(`Loading raw JSON from ${fullPath}`)
+
     try {
+        // load file synchronously and parse to JSON
         const json = readFileSync(fullPath, "utf8");
         const data = JSON.parse(json);
-        console.log(data);
+        return data
     } catch (err) {
-        console.log(err);
+        // report any errors caught and return null
+        console.error(`Error parsing ${fullPath}:`, err);
+        return null
     }
 }
--- src/tests/readTest.js
+++ src/tests/readTest.js
@@ -1,9 +1,9 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 26 Jul 2024, 5:22:09 PM
- *  Last update: 29 Jul 2024, 12:26:01 PM
+ *  Last update: 29 Jul 2024, 1:14:11 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { loadRawJson } from "../files/read.js";
 
-loadRawJson("Data", "Boots");
+console.log(loadRawJson("Data", "Crops"));
