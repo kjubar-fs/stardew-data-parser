d738c4bc8eac8ae8dcb928fe148109ce90a77622
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-07-29 12:18:46 -0400

    Add skeleton for JSON data reading

--- src/files/read.js
+++ src/files/read.js
@@ -1,14 +1,30 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 26 Jul 2024, 5:17:35 PM
- *  Last update: 26 Jul 2024, 5:54:15 PM
+ *  Last update: 29 Jul 2024, 11:46:25 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
+import path from "node:path";
+import { readFileSync } from "node:fs";
+
+const DATA_DIRECTORY = "../../raw-data-escaped";
+const curDir = import.meta.dirname
+
 /**
- * Load a raw JSON file as an object 
- * @param dir directory to load from
- * @param file filename to load (no extension)
+ * Load a raw JSON file as an object
+ * @param {string} dir directory to load from
+ * @param {file} file filename to load (no extension)
  */
-export function loadRawJson(dir: String, file: String): any | null {
-    console.log(`Loading raw JSON from ${dir}/${file}.escaped.json...`)
+export function loadRawJson(dir, file) {
+    const filename = `${file}.escaped.json`;
+    const fullPath = path.join(curDir, DATA_DIRECTORY, dir, filename);
+    console.log(`Loading raw JSON from ${fullPath}`)
+    try {
+        const json = readFileSync(fullPath, "utf8");
+        console.log(json);
+        const data = JSON.parse(json);
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
 }
---
+++ src/tests/readTest.js
@@ -0,0 +1,9 @@
+/*
+ *  Author: Kaleb Jubar
+ *  Created: 26 Jul 2024, 5:22:09 PM
+ *  Last update: 26 Jul 2024, 7:07:25 PM
+ *  Copyright (c) 2024 Kaleb Jubar
+ */
+import { loadRawJson } from "../files/read.js";
+
+loadRawJson("Data", "Boots");
