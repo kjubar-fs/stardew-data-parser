61744af48b6088e410df557857f9ae49ffe2792f
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-07-31 12:12:44 -0400

    Add seasons enum

--- src/globals.js
+++ src/globals.js
@@ -1,12 +1,19 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 30 Jul 2024, 12:29:38 PM
- *  Last update: 30 Jul 2024, 12:40:14 PM
+ *  Last update: 31 Jul 2024, 12:12:25 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
-export const DEBUG = true;
+export const DEBUG = false;
 
 export const DATA_DIRECTORY = "Data";
 export const STRINGS_DIRECTORY = "Strings";
 export const ROOT_DATA_DIRECTORY = "../../raw-data-escaped";
 export const OUTPUT_DIRECTORY = "../../output-data";
+
+export const SEASONS = Object.freeze({
+    "1": "spring",
+    "2": "summer",
+    "3": "fall",
+    "4": "winter"
+});
