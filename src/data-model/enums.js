5ee0cf775a965cff429eac6b3e3797ac74fee6dc
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-09-01 18:53:57 -0400

    Add production-related enums

---
+++ src/data-model/enums.js
@@ -0,0 +1,46 @@
+/*
+ *  Author: Kaleb Jubar
+ *  Created: 1 Sep 2024, 6:42:25 PM
+ *  Last update: 1 Sep 2024, 6:53:17 PM
+ *  Copyright (c) 2024 Kaleb Jubar
+ */
+export const PROD_SRCS = Object.freeze({
+    // appliances
+    appliances: {
+        jar: "preserves jar",
+        keg: "keg",
+        mayo: "mayonnaise machine",
+        cheese: "cheese press",
+        oil: "oil maker",
+        loom: "loom",
+        bee: "bee house",
+        smoker: "fish smoker",
+        dehydrate: "dehydrator",
+    },
+
+    // animals
+    coop: {
+        chxWhite: "white chicken",
+        chxBrown: "brown chicken",
+        chxGold: "golden chicken",
+        chxVoid: "void chicken",
+        duck: "duck",
+        rabbit: "rabbit",
+        dino: "dinosaur",
+    },
+    
+    barn: {
+        cow: "cow",
+        pig: "pig",
+        goat: "goat",
+        sheep: "sheep",
+        ostrich: "ostritch",
+    },
+});
+
+export const PROD_UNITS = Object.freeze({
+    mins: "minutes",
+    nights: "nights",
+    day: "day",
+    days: "days",
+});
